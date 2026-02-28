972375329f4a75c82d72724c81f9a9ac0102b902
Akkun7 <Akkun752@gmail.com>
2026-02-28 13:40:57 +0100

    v0.0.3: add MissingNo., remove /akkun & /panel

--- scopikun_bot.py
+++ scopikun_bot.py
@@ -17,7 +17,7 @@ import scopikun_db
 load_dotenv()
 
 # Configuration
-VERSION = "v0.0.2"
+VERSION = "v0.0.3"
 print(f"Lancement du bot Scopikun {VERSION}...")
 
 class MyBot(commands.Bot):
@@ -55,32 +55,10 @@ async def help_command(interaction: discord.Interaction): # Retrait de self
         description="Voici la liste des commandes disponibles avec Scopikun !",
         color=discord.Color.red()
     )
-    embed.add_field(name="/akkun", value="Affiche les chaînes Akkun7", inline=False)
-    embed.add_field(name="/panel", value="Affiche les liens d'Astero", inline=False)
     embed.add_field(name="/pokedex", value="Affiche la page Pokédex d'un Pokémon", inline=False)
 
     if interaction.user.guild_permissions.administrator:
         embed.add_field(name="━━━━━━━━━━━━━━━━━━━━", value="**Commandes Administrateurs**", inline=False)
-    await interaction.response.send_message(embed=embed)
-
-# === Commande /akkun ===
-@bot.tree.command(name="akkun", description="Affiche les chaînes Akkun7")
-async def akkun(interaction: discord.Interaction): # Retrait de self
-    embed = discord.Embed(title="Akkun", description="**Voici les chaînes de Akkun :**", color=discord.Color.red())
-    embed.set_thumbnail(url="https://www.akkunverse.fr/scopikun/akkun.png")
-    embed.add_field(name="🎥 YouTube", value="https://youtube.com/@Akkun7", inline=False)
-    embed.add_field(name="🎬 YouTube VOD", value="https://youtube.com/@Akkun7VOD", inline=False)
-    embed.add_field(name="👾 Twitch", value="https://twitch.tv/akkun752", inline=False)
-    embed.add_field(name="🤖 Discord", value="https://discord.gg/24kM8KUd9j", inline=False)
-    await interaction.response.send_message(embed=embed)
-
-# === Commande /panel ===
-@bot.tree.command(name="panel", description="Affiche les liens de Scopikun")
-async def panel(interaction: discord.Interaction): # Retrait de self
-    embed = discord.Embed(title="Scopikun", description="**Voici les différents liens de Scopikun :**", color=discord.Color.red())
-    embed.set_thumbnail(url="https://www.akkunverse.fr/scopikun/ScopikunBot.png")
-    embed.add_field(name="🤖 Discord", value="``- À venir -``", inline=False)
-    embed.add_field(name="🌐 Site web", value="``- À venir -``", inline=False)
     await interaction.response.send_message(embed=embed)
 
 # === Commande /pokedex ===
