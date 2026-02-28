08cf9a970ac5c0fae05a5d2ca9b1116cd28e0f38
Akkun7 <Akkun752@gmail.com>
2026-02-28 13:00:47 +0100

    v0.0.2: /pokedex

--- scopikun_bot.py
+++ scopikun_bot.py
@@ -8,13 +8,16 @@ import asyncio
 import feedparser
 import aiohttp
 import random
+import aiohttp
+import io
+from PIL import Image
 
 import scopikun_db
 
 load_dotenv()
 
 # Configuration
-VERSION = "v0.0.1"
+VERSION = "v0.0.2"
 print(f"Lancement du bot Scopikun {VERSION}...")
 
 class MyBot(commands.Bot):
@@ -54,6 +57,7 @@ async def help_command(interaction: discord.Interaction): # Retrait de self
     )
     embed.add_field(name="/akkun", value="Affiche les chaînes Akkun7", inline=False)
     embed.add_field(name="/panel", value="Affiche les liens d'Astero", inline=False)
+    embed.add_field(name="/pokedex", value="Affiche la page Pokédex d'un Pokémon", inline=False)
 
     if interaction.user.guild_permissions.administrator:
         embed.add_field(name="━━━━━━━━━━━━━━━━━━━━", value="**Commandes Administrateurs**", inline=False)
@@ -79,5 +83,67 @@ async def panel(interaction: discord.Interaction): # Retrait de self
     embed.add_field(name="🌐 Site web", value="``- À venir -``", inline=False)
     await interaction.response.send_message(embed=embed)
 
+# === Commande /pokedex ===
+@bot.tree.command(name="pokedex", description="Affiche les informations d'un Pokémon")
+@app_commands.describe(
+    pokemon="Nom (FR/EN) ou numéro du Pokémon",
+    forme="Optionnel : Alola, Galar, Hisui, Paldea (ou A, G, H, P)"
+)
+async def pokedex(interaction: discord.Interaction, pokemon: str, forme: str = None):
+    # recup db
+    data = scopikun_db.get_pokemon_data(pokemon, forme)
+    if not data:
+        await interaction.response.send_message(
+            f"❌ Impossible de trouver le Pokémon **{pokemon}**" + 
+            (f" sous la forme **{forme}**." if forme else "."), 
+            ephemeral=True
+        )
+        return
+
+    # image
+    num = str(data['num_dex']).zfill(4)
+    code_forme = data['forme'] if data['forme'] else ""
+    filename = f"{num}{code_forme}.png"
+    path_image = f"./sprites/{filename}"
+    if not os.path.exists(path_image):
+        await interaction.response.send_message(f"❌ Image locale manquante : `{filename}`", ephemeral=True)
+        return
+    file = discord.File(path_image, filename="pokemon.png")
+
+    # embed
+    embed = discord.Embed(
+        title=f"#{num} - {data['nom_fr']}",
+        description=data['description'],
+        color=discord.Color.red()
+    )
+    embed.set_thumbnail(url="attachment://pokemon.png")
+    type_str = data['type_1'] + (f" / {data['type_2']}" if data['type_2'] else "")
+    embed.add_field(name="Types", value=type_str, inline=True)
+    embed.add_field(name="Nom Anglais", value=data['nom_en'], inline=True)
+
+    if data['forme']:
+        if data['forme'] == "A":
+            formname = "Alola"
+        elif data['forme'] == "G":
+            formname = "Galar"
+        elif data['forme'] == "H":
+            formname = "Hisui"
+        elif data['forme'] == "P":
+            formname = "Paldea"
+        else:
+            formname = data['forme']
+        embed.add_field(name="Forme", value=formname, inline=True)
+    stats = (
+        f"**PV:** {data['hp']}\n"
+        f"**ATK:** {data['atk']}\n"
+        f"**DEF:** {data['def']}\n"
+        f"**SPA:** {data['spa']}\n"
+        f"**SPD:** {data['spd']}\n"
+        f"**SPE:** {data['spe']}"
+    )
+    embed.add_field(name="Statistiques de base", value=stats, inline=False)
+
+    await interaction.response.send_message(embed=embed, file=file)
+    
 # === Lancer le bot ===
 bot.run(os.getenv("DISCORD_TOKEN"))
